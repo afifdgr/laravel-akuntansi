4a817fee00c8ee2aa6c452eabf92c9e9e1375756
afifdgr <afifdgr3@gmail.com>
2022-11-09 11:20:56 +0700

    # API Endpoint Index

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TransactionController extends Controller
+{
+    public function index()
+    {
+        $transaction = Transaction::orderBy('time', 'DESC')->get();
+        $response = [
+            'message' => 'List transaction order by time',
+            'data' => $transaction
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
